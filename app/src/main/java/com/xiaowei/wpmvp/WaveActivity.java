9dfd748a198c98869a2ecf616ea869419b28491e
weip <wdzaxiaowei@126.com>
2020-04-13 22:42:55 +0800

    commit wave acitivity

--- app/src/main/java/com/xiaowei/wpmvp/WaveActivity.java
+++ app/src/main/java/com/xiaowei/wpmvp/WaveActivity.java
@@ -2,10 +2,13 @@ package com.xiaowei.wpmvp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 
 import com.xiaowei.wpmvp.view.WaveView;
+import com.xiaowei.wpmvp.view.WaveView1;
+import com.xiaowei.wpmvp.view.WaveView2;
 
 public class WaveActivity extends Activity {
 
@@ -13,7 +16,6 @@ public class WaveActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.wave_layout);
-        WaveView waveView=findViewById(R.id.waveView);
-        waveView.startAnimation();
+        WaveView1 waveView=findViewById(R.id.waveView);
     }
 }
