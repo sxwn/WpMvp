6fe6c3506af9d7649f28cce068c69653f5c3ee5f
weip <wdzaxiaowei@126.com>
2020-05-27 22:00:57 +0800

    viewpager content 高度不生效的问题

--- app/src/main/java/com/xiaowei/wpmvp/BannerActivity.java
+++ app/src/main/java/com/xiaowei/wpmvp/BannerActivity.java
@@ -22,6 +22,12 @@ import androidx.appcompat.app.AppCompatActivity;
  * activity view window的关系?phonewindow decorview titleActionBar
  * setContent
  * 博客都对吗？对的---》学习博客解决问题的原理，解决问题的思路就是看源码，分析源码
+ * 原理----  竞品，产品  设计  学到了什么？  面试官问你，你知道吗？
+ * recycleview源码 viewpager源码  measurespec是干吗的  xml解析过程
+ * 自定义view  布局，显示（cavans），事件分发   动手
+ * glide源码:白屏、黑屏、图片加载缓存、缓存、网络层进行定制(ua),第一次加载所有配置的module，RequestManager网络层，工作线程，Options（300ms）
+ * SplashActivity:广告图  glide加载  ，加载的时候无论是本地还是网络的，第一次使用的时候，这张图片加载都会很慢
+ * okhttp请求源码:
  */
 
 public class BannerActivity extends AppCompatActivity {
